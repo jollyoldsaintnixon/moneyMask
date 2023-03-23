c7c2075ba03df7bb49f0c6597c206b7a56699ca2
Jolly Old Saint Nixon <jollyoldsaintnixon@pop-os.localdomain>
2023-03-23 18:08:32 -0300

    background service worker swaps between match/noMatch icons

--- background.js
+++ background.js
@@ -1,31 +1,47 @@
 const targetDomainRegexList = [
-    'fidelity.com/',
-    'robinhood.com/',
-].map(domain => new RegExp(`^https?://([a-zA-Z0-9-]+\.)?${domain}/`));
+    'fidelity.com',
+    'robinhood.com',
+].map(domain => new RegExp(`^https?://([a-zA-Z0-9-]+\.)?${escapeRegExp(domain)}`));
 
 console.log('hey background.js');
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete') {
-      if (targetDomainRegexList.some((domainRegex) => domainRegex.test(tab.url))) {
-        // Update the icon
-        chrome.action.setIcon({
-            path: {
-              '16': "icons/banditMask.png",
-              '48': "icons/banditMask.png",
-              '128': "icons/banditMask.png",
-            },
-            tabId: tabId
-          })
-      } else {
-        chrome.action.setIcon({
-            path: {
-              '16': "icons/noMatch.png",
-              '48': "icons/noMatch.png",
-              '128': "icons/noMatch.png",
-            },
-            tabId: tabId
-          })
-      }
+function updateIcon(tab) {
+    // test for a pattern match
+    let iconFileBase;
+    console.log("tab.url: ", tab.url)
+    if (targetDomainRegexList.some((domainregex) => domainregex.test(tab.url))) {
+        iconFileBase = "icons/banditMask" // match
     }
-  });
+    else {
+        iconFileBase = "icons/noMatch" // no match
+    }
+
+    // set icon
+    chrome.action.setIcon({
+        path: {
+            '16': iconFileBase + "-16.png",
+            '48': iconFileBase + "-48.png",
+            '128': iconFileBase + "-128.png",
+        },
+        tabId: tab.id
+    })
+}
+
+function handleTabUpdated(tabId, changeInfo, tab) {
+    if (changeInfo.status === 'complete') {
+      updateIcon(tab);
+    }
+}
+
+function handleTabActivated(activeInfo) {
+    chrome.tabs.get(activeInfo.tabId, (tab) => {
+      updateIcon(tab);
+    });
+  }
+
+chrome.tabs.onUpdated.addListener(handleTabUpdated);
+chrome.tabs.onActivated.addListener(handleTabActivated);
+
+function escapeRegExp(string) {
+    return string.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&');
+}
--- scripts/iconMaker.sh
+++ scripts/iconMaker.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
 # pass in the file you want that will serve as a template for the icons
-# do not include the extension
 
-convert $1.png -resize 16x16\! $1_16.png;
-convert $1.png -resize 48x48\! $1_48.png;
-convert $1.png -resize 128x128\! $1_128.png; 
+# check if the input argument is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <image_file>"
+    exit 1
+fi
+
+# check if the input file exists
+if [ ! -f "$1" ]; then
+    echo "File not found: $1"
+    exit 1
+fi
+
+# strip out the extension
+# remove the shortest suffix after the last '.'
+name="${1%.*}"
+# remove the longest prefix before the first '.'
+ext="${1##*.}"
+
+# set the output dimensions
+dimensions=("16" "48" "128")
+
+# loop through the dimensions and resize the image
+for dim in "${dimensions[@]}"; do
+    convert ${name}.${ext} -resize ${dim}x${dim}\! ${name}-${dim}.${ext};
+done
