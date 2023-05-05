97f6a296c72062bbde5d160c5c21fea002e9ceec
jOSN <tlmcmerty+gitkraken@gmail.com>
2023-05-05 14:36:25 -0400

    added PanelIra to widget map (so it will work from this commit on)

--- classes/UrlsToWidgetsMaps/FidelityUrlsToWidgetsMap.js
+++ classes/UrlsToWidgetsMaps/FidelityUrlsToWidgetsMap.js
@@ -1,10 +1,12 @@
-import SummaryBodyTotalWidget from "../fidelity/SummaryBodyTotalWidget";
+import PanelIraWidget from "../fidelity/PanelIraWidget";
+import PanelTotalWidget from "../fidelity/PanelTotalWidget";
 import SummarySidebarWidget from "../fidelity/SummarySidebarWidget";
 
 export default {
     "http[s]?:\/\/.+fidelity\.com\/.*portfolio\/summary": [
         SummarySidebarWidget, 
-        SummaryBodyTotalWidget,
+        PanelTotalWidget,
+        PanelIraWidget,
     ],
     "http[s]?:\/\/.+fidelity\.com\/.*portfolio\/(positions|summary)": [
         SummarySidebarWidget,
