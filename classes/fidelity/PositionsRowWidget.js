e82b7e646306c6bd8de793b40ba8072ff65932ee
jOSN <tlmcmerty+gitkraken@gmail.com>
2023-05-09 14:15:52 -0400

    fixed PositionsRowWidget
    - i wasn't looking at the correct percents for certain nodes within a security row
    -- had to do away with the programatically way of masking/resetting it
    - i now account for the final "account total" rows

--- classes/fidelity/PositionsRowWidget.js
+++ classes/fidelity/PositionsRowWidget.js
@@ -11,16 +11,20 @@ import {
  */
 export default class PositionsRowWidget extends OnlySecondaryWidgetBase
 {
-    targetNodeSelector = ".ag-center-cols-container > div.ag-row:not(.ag-row-first)"; // this will return a series of security rows. Different nodes within each row will be masked based on the percent of the total account represented by each security.
+    targetNodeSelector = ".ag-center-cols-container > div.ag-row:not(.ag-row-first, .posweb-row-spacer)"; // this will return a series of security rows. Different nodes within each row will be masked based on the percent of the total account represented by each security.
     targetCommonAncestorSelector = ".ag-body-viewport"
     percentOfAccountSelector = ".posweb-cell-account_percent > div:first-child > span:first-child"; // this is the percent of the account represented by the security
+    percentTotalGainLossSelector = ".posweb-cell-total_gl_percent > div:first-child > span:first-child"; // this is the percent for total gain/loss for the security
+    percentTodaysGainLossSelector = ".posweb-cell-today_gl_percent > div:first-child > span:first-child"; // this is the percent for today's gain/loss for the security
     /*** Secondary selectors for each row ***/
     totalGainLossSelector = ".posweb-cell-total_gl_currency > div:first-child > span:first-child"; // this is the total gain/loss for the security
     todaysGainLossSelector = ".posweb-cell-today_gl_currency > div:first-child > span:first-child"; // this is the gain/loss for the security today
     currentValueSelector = ".posweb-cell-current_value > div:first-child > span:first-child"; // this is the current value of the security
     quantitySelector = ".posweb-cell-quantity > div:first-child > span:first-child"; // this is the quantity of the security
     costBasisTotalSelector = ".posweb-cell-cost_basis > div:first-child > span:first-child"; // this is the cost basis of the security
-
+    /**** *****/
+    dudRowClass = "posweb-row-spacer"; // this is a row that is not a security row. It is used to space out the securities in the list.
+    
     constructor(maskValue = 100, isMaskOn = false) 
     {
         super(maskValue, isMaskOn);
@@ -33,32 +37,94 @@ export default class PositionsRowWidget extends OnlySecondaryWidgetBase
     {
         for (const securityNode of this.getTargetNodes())
         {
-            const percentOfAccount = this.getPercentOfAccount(securityNode);
-            if (percentOfAccount > 0) // ensure the percent was found (it won't be on header rows)
+            // the first two are each based on a different percent and are masked the same for both account total and security rows
+            this.maskTotalGainLoss(securityNode);
+            this.maskTodaysGainLoss(securityNode);
+            // determin if account total row or normal security row
+            if (this.isAccountTotalRow(securityNode))
             {
-                this.getSecondaryNodesWithDollarFunc(securityNode).forEach(({ node, dollarFunc }) => {
-                    if (node)
-                    {
-                        this.maskSecondaryNode(node, percentOfAccount, dollarFunc);
-                    }
-                });
-            }
+                this.maskAccountTotalRow(securityNode);
+            } 
+            else
+            {
+                this.maskSecurityRow(securityNode);
+            } 
+        }
+    }
+
+    maskTotalGainLoss(securityNode)
+    {
+        const totalGainLossNode = securityNode.querySelector(this.totalGainLossSelector);
+        const percentNode = securityNode.querySelector(this.percentTotalGainLossSelector);
+        if (this.validNodes(totalGainLossNode, percentNode))
+        {
+            this.maskPositionNode(totalGainLossNode, stripToNumber(percentNode.textContent), toGainDollars);
+        }
+    }
+
+    maskTodaysGainLoss(securityNode)
+    {
+        const todaysGainLossNode = securityNode.querySelector(this.todaysGainLossSelector);
+        const percentNode = securityNode.querySelector(this.percentTodaysGainLossSelector);
+        if (this.validNodes(todaysGainLossNode, percentNode))
+        {
+            this.maskPositionNode(todaysGainLossNode, stripToNumber(percentNode.textContent), toGainDollars);
+        }
+    }
+
+    maskAccountTotalRow(securityNode)
+    {
+        securityNode.querySelector(this.currentValueSelector).textContent = toDollars(this.maskValue);
+    }
+
+    maskSecurityRow(securityNode)
+    {
+        const percentNode = securityNode.querySelector(this.percentOfAccountSelector); // these next three masks are all based on this percent
+        this.maskCurrentValue(securityNode, percentNode);
+        this.maskQuantity(securityNode, percentNode);
+        this.maskCostBasisTotal(securityNode, percentNode);
+    }
+
+    maskCurrentValue(securityNode, percentNode)
+    {
+        const currentValueNode = securityNode.querySelector(this.currentValueSelector);
+        percentNode = percentNode ?? securityNode.querySelector(this.percentOfAccountSelector);
+        if (this.validNodes(currentValueNode, percentNode))
+        {
+            this.maskPositionNode(currentValueNode, stripToNumber(percentNode.textContent), toDollars);
+        }
+    }
+
+    maskQuantity(securityNode, percentNode)
+    {
+        percentNode = percentNode ?? securityNode.querySelector(this.percentOfAccountSelector);
+        const quantityNode = securityNode.querySelector(this.quantitySelector);
+        if (this.validNodes(quantityNode, percentNode))
+        {
+            this.maskPositionNode(quantityNode, stripToNumber(percentNode.textContent));
+        }
+    }
+
+    maskCostBasisTotal(securityNode, percentNode)
+    {
+        percentNode = percentNode ?? securityNode.querySelector(this.percentOfAccountSelector);
+        const costBasisTotalNode = securityNode.querySelector(this.costBasisTotalSelector);
+        if (this.validNodes(costBasisTotalNode, percentNode))
+        {
+            this.maskPositionNode(costBasisTotalNode, stripToNumber(percentNode.textContent), toDollars);
         }
     }
 
     /**
      * 
-     * @param {Node} node 
-     * @param {float} percentOfAccount 
-     * @param {function} dollarFunc 
+     * @param {Node} node to be masked
+     * @param {float} percent percent to base mask on
+     * @param {function} dollarFunc toDollars, toGainDollars, etc. defaults to returning an unmodified value.
      */
-    maskSecondaryNode(node, percentOfAccount, dollarFunc)
+    maskPositionNode(node, percent, dollarFunc = (value) => value)
     {
-        if (!isNaN(stripToNumber(node.textContent))) // sometimes rows don't have a number, such as with money markets. pass if so.
-        {
-            const maskedValue = dollarFunc(this.maskValue * percentOfAccount / 100);
-            WidgetBase.maskUp(node, maskedValue);
-        }
+        const maskedValue = dollarFunc(this.maskValue * percent / 100);
+        WidgetBase.maskUp(node, maskedValue);
     }
 
     /******************** RESETTERS **********************/
@@ -68,60 +134,32 @@ export default class PositionsRowWidget extends OnlySecondaryWidgetBase
     {
         for (const securityNode of this.getTargetNodes())
         {
-            this.getSecondaryNodesWithDollarFunc(securityNode).forEach(({ node }) => {
-                WidgetBase.unmask(node);
-            });
+            WidgetBase.unmask(securityNode.querySelector(this.totalGainLossSelector));
+            WidgetBase.unmask(securityNode.querySelector(this.todaysGainLossSelector));
+            WidgetBase.unmask(securityNode.querySelector(this.currentValueSelector));
+            WidgetBase.unmask(securityNode.querySelector(this.quantitySelector));
+            WidgetBase.unmask(securityNode.querySelector(this.costBasisTotalSelector));
         }
     }
 
     /******************** GETTERS **********************/
+    
+    /******************** OTHERS **********************/
 
     /**
-     * Returns the percent of the total account represented by the security. If no percent is found, returns -1.
-     * @param {Node} securityNode 
-     * @returns {float|null}
+     * Verify that the position node should be masked
+     * @param {Node} positionNode the node to be masked
+     * @param {Node} percentNode the node holding the percent to base the mask on
+     * @returns 
      */
-    getPercentOfAccount(securityNode)
+    validNodes(positionNode, percentNode)
     {
-        const percentOfAccountNode = securityNode.querySelector(this.percentOfAccountSelector);
-        if (percentOfAccountNode && percentOfAccountNode.textContent.length)
-        {
-            return stripToNumber(percentOfAccountNode.textContent);
-        }
-        else
-        {
-            return -1;
-        }
+        return ((positionNode && !isNaN(stripToNumber(positionNode.textContent))) 
+            && (percentNode && percentNode.textContent.length));
     }
 
-    /**
-     * Returns an object containing the node and dollar function for each secondary node.
-     * @param {Node} securityNode
-     * @returns {Node[]} An array of nodes that are masked based on the percent of the total account represented by each security. 
-     */
-    getSecondaryNodesWithDollarFunc(securityNode)
+    isAccountTotalRow(securityNode)
     {
-        const secondaryNodesWithDollarFuncs = [];
-        secondaryNodesWithDollarFuncs.push({
-            node: securityNode.querySelector(this.totalGainLossSelector),
-            dollarFunc: toGainDollars,
-        });
-        secondaryNodesWithDollarFuncs.push({
-            node: securityNode.querySelector(this.todaysGainLossSelector),
-            dollarFunc: toGainDollars
-        });
-        secondaryNodesWithDollarFuncs.push({
-            node: securityNode.querySelector(this.currentValueSelector),
-            dollarFunc: toDollars,
-        });
-        secondaryNodesWithDollarFuncs.push({
-            node: securityNode.querySelector(this.quantitySelector),
-            dollarFunc: (value) => value, 
-        });
-        secondaryNodesWithDollarFuncs.push({
-            node: securityNode.querySelector(this.costBasisTotalSelector),
-            dollarFunc: toDollars
-        });
-        return secondaryNodesWithDollarFuncs;
+        return (securityNode.nextSibling && securityNode.nextSibling.classList.contains(this.dudRowClass));
     }
 }
